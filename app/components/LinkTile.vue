71bce7f69e7fec8f5c7ed2a7480c3a60dfb5e889
FormalistBlue <FormalistBlue@users.noreply.github.com>
2026-07-18 22:45:30 +0800

    fix: simplify shortcut hover actions

--- app/components/LinkTile.vue
+++ app/components/LinkTile.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Grip, MoreHorizontal } from '@lucide/vue'
+import { MoreHorizontal } from '@lucide/vue'
 import type { LinkItem } from '#shared/contracts'
 
 const props = defineProps<{ link: LinkItem; iconSize: number }>()
@@ -26,15 +26,13 @@ function open(event: MouseEvent) {
     @keydown.enter="open($event as unknown as MouseEvent)"
   >
     <div class="link-tile__actions">
-      <button class="icon-button icon-button--micro link-tile__drag" type="button" aria-label="拖动排序">
-        <Grip :size="14" />
-      </button>
       <button class="icon-button icon-button--micro" type="button" :aria-label="`编辑 ${link.title}`" @click.stop="emit('edit', link)">
         <MoreHorizontal :size="15" />
       </button>
     </div>
     <div
-      class="link-icon"
+      class="link-icon link-tile__drag"
+      title="拖动排序"
       :style="{ '--icon-size': `${iconSize}px`, '--icon-color': link.iconColor }"
     >
       <img v-if="link.iconPath" :src="link.iconPath" alt="" loading="lazy">
